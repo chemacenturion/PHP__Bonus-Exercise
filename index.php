658f3be05d6769543f7e044aceb3112b9e2879c0
chemacenturion <centurion.philadelphia@gmail.com>
2022-02-25 21:35:20 -0500

    cont troubleshooting; names populate on screen

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
     <?php
+        if (isset($_POST["submit"])) {
+        $searchValue = $_POST["search"];
         $conn = new mysqli("localhost","root","","directory_db");
+        if ($conn->connect_error){
+            echo "Connection failed: ". $conn->connect_error;
+        } else {
 
-        // write query for all customers
-        $sql = "SELECT * FROM customers ORDER BY id";
+        $sql = "SELECT * FROM customers WHERE first_name OR last_name LIKE '%$searchValue%'";
 
-        // make query and get results
-        $result = mysqli_query($conn, $sql);
-
-        // fetch the resulting rows as an array
-        $customers = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-        print_r($customers);
+        $result = $conn->query($sql);
+        while ($row = $result->fetch_assoc()) {
+            echo $row['first_name'] . "&nbsp" . $row['last_name'] . "<br>";
+        }
+        }
+    }
     ?>
 
 <html lang="en">
@@ -20,14 +23,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Bonus Exercise</title>
+    <link rel="stylesheet" href="./assets/styles.css">
 </head>
 <body>
-    <h1>customers</h1>
-        <div>
-            <?php foreach($customers as $customer){ ?>
-            <h5><?php echo htmlspecialchars($customer["first_name"]); ?></h5>
-        </div>
-
-        <?php } ?>
+    <h1>Name Search with PHP and MySQL</h1>
+    <table>
+        <tr>
+            <td id="table-form">
+                <form action="" method="post">
+                    Search: <input type="text" placeholder="Search" name="search">
+                    <input type="submit" name="submit" value="search">
+                </form>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
