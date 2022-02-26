c3637dc852bee47aec307942d9aa172bd5b76b03
chemacenturion <centurion.philadelphia@gmail.com>
2022-02-26 17:15:37 -0500

    project finished

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
-# PHP__Bonus-Exercise
+## PHP Bonus Exercise ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
 
-My first PHP/Apache/MySQL build in progress!
+## GIF
+Below you will find a GIF animation of the working code. In order to see this live in the browser you would need to clone the code down yourself and set up the Apache Environment. I was tasked with XAMPP:
 
-![PHP Progress](https://github.com/chemacenturion/PHP__Bonus-Exercise/blob/main/assets/images/PHP%20Bonus%20Exercise.gif?raw=true)
+## Description
+This is a bonus technical interview project I was tasked with to test if I could figure out how to connect a MySQL database with PHP running on an Apache Server and query the proper results in the browser.
 
-2/25/22 - Currently everything is connected in the browser, but the submit button returns all the names in the database. Need to troubleshoot my syntax to achieve the desired results listed below:
+## Table of Contents:
+* [GIF](#GIF)
+* [Description](#Description)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Questions](#Questions)
 
-Exercise:
-Build a search page that allows a user to search by first name or last name from the mockdata.sql file and returns the intended results.
+## Usage
+Technical Interview
 
-Expected outcomes:
-Input:
-	Kyle
-Output:
-	Kyle Smith Baton Rouge LA
-	Kyle Bonehill Baton Rouge LA
-	
-Input:
-	Ivashov
-Output:
-	Lorri Ivashov San Francisco CA
-	Steve Ivashov San Francisco CA
-	
-Input:
-	Ewen
-Output:
-	Ewen Dike Phoenix AZ
-	Steven Ewen Phoenix AZ
+## License
+Apache 2.0 Copyright: Jose Centurion 2022
+<br/>
+Licensed under the Apache License, Version 2.0 (the "License"); <br/> you may not use this file except in compliance with the License. <br/> You may obtain a copy of the License at: <br/> http://www.apache.org/licenses/LICENSE-2.0 <br/> Unless required by applicable law or agreed to in writing, software <br/> distributed under the License is distributed on an "AS IS" BASIS, <br/> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br/> See the License for the specific language governing permissions and <br/> limitations under the License.
+
+## Contributing
+No outside contributions are being accepted at this time.
+
+## Questions
+Please reach out to me with any further questions via email at:
+<br/>
+paraguayandeveloper@gmail.com
+<br/>
+You can follow my work at my Github listed below:
+<br/>
+https://github.com/chemacenturion
+
--- index.php
+++ index.php
@@ -4,36 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pushster&display=swap" rel="stylesheet">
     <title>PHP Bonus Exercise</title>
     <link rel="stylesheet" href="./assets/styles.css"/>
 </head>
 <body>
-    <h1>Name Search with PHP and MySQL</h1>
+    <nav class="nav__container">
+        <h1>Name Search with PHP and MySQL</h1>
+    </nav>
     <div>
-        <div id="container">
-            <form action="" method="post">
-                Search: <input type="text" placeholder="Search" name="search">
-                <input type="submit" name="submit" value="search">
+        <div class="container__assignment">
+            <div class="container__assignment-text">
+                <h2>Exercise</h2>
+                <p>Build a search page that allows a user to search by first name or last name from the mockdata.sql file and returns the intended results.</p>
+                <h3>Expected Outcomes</h3>
+                <p>Input: Kyle => Output: Kyle Smith Baton Rouge LA Kyle Bonehill Baton Rouge LA</p>
+                <p>Input: Ivashov => Output: Lorri Ivashov San Francisco CA Steve Ivashov San Francisco CA</p>
+                <p>Input: Ewen => Output: Ewen Dike Phoenix AZ Steven Ewen Phoenix AZ</p>
+            </div>
+        </div>
+        <div class="container__search">
+            <form class="container__search-form" action="" method="post">
+                Search: <input type="text" placeholder="Search Names Here" name="search">
+                <input type="submit" name="submit" value="Submit">
             </form>
         </div>
-</div>
+        <div class="container__results">
+            <?php
+                if (isset($_POST["submit"])) {
+                $searchValue = $_POST["search"];
+                $conn = new mysqli("localhost","root","","directory_db");
+                    if ($conn->connect_error){
+                        echo "Connection failed: ". $conn->connect_error;
+                    } else {
+
+                        $sql = "SELECT * FROM customers WHERE first_name LIKE '%$searchValue%' OR last_name LIKE '%$searchValue%'";
+
+                        $result = $conn->query($sql);
+                        while ($row = $result->fetch_assoc()) {
+                            echo $row['first_name'] . "&nbsp" . $row['last_name'] . "&nbsp" . $row['city'] . "&nbsp" . $row['state'] . "<br>";
+                        }
+                    }
+                }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
-
-<?php
-        if (isset($_POST["submit"])) {
-        $searchValue = $_POST["search"];
-        $conn = new mysqli("localhost","root","","directory_db");
-        if ($conn->connect_error){
-            echo "Connection failed: ". $conn->connect_error;
-        } else {
-
-        $sql = "SELECT * FROM customers WHERE first_name OR last_name LIKE '%$searchValue%'";
-
-        $result = $conn->query($sql);
-        while ($row = $result->fetch_assoc()) {
-            echo $row['first_name'] . "&nbsp" . $row['last_name'] . "<br>";
-        }
-        }
-    }
-?>
